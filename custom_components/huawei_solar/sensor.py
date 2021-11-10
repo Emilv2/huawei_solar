e31f2e4a2c332c85205f3c4d289d0dde5bb47cab
Emil Vanherp <emil@vanherp.me>
2021-11-10 19:24:37 +0100

    black and isort

--- custom_components/huawei_solar/sensor.py
+++ custom_components/huawei_solar/sensor.py
@@ -1,28 +1,28 @@
-import logging
 import asyncio
-import homeassistant.helpers.config_validation as cv
+import logging
 from datetime import timedelta
+
+import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
 from homeassistant.components.sensor import (
-    PLATFORM_SCHEMA,
-    STATE_CLASS_MEASUREMENT,
     ATTR_LAST_RESET,
     ATTR_STATE_CLASS,
+    PLATFORM_SCHEMA,
+    STATE_CLASS_MEASUREMENT,
 )
-
 from homeassistant.const import (
     CONF_HOST,
-    POWER_WATT,
-    ENERGY_KILO_WATT_HOUR,
     DEVICE_CLASS_CURRENT,
     DEVICE_CLASS_ENERGY,
     DEVICE_CLASS_POWER,
     DEVICE_CLASS_VOLTAGE,
+    ENERGY_KILO_WATT_HOUR,
+    POWER_WATT,
 )
 from homeassistant.helpers.entity import Entity
 from homeassistant.util.dt import utc_from_timestamp
 
-from huawei_solar import AsyncHuaweiSolar, ReadException, ConnectionException
+from huawei_solar import AsyncHuaweiSolar, ConnectionException, ReadException
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -46,67 +46,67 @@ ATTR_NB_PV_STRINGS = "nb_pv_strings"
 
 
 STATIC_ATTR_LIST = [
-"model_id",
-"model_name",
-ATTR_SERIAL_NUMBER,
-"rated_power",
-ATTR_NB_PV_STRINGS,
+    "model_id",
+    "model_name",
+    ATTR_SERIAL_NUMBER,
+    "rated_power",
+    ATTR_NB_PV_STRINGS,
 ]
 
 STATIC_ATTR_GRID_LIST = [
-"grid_standard",
-"grid_country",
+    "grid_standard",
+    "grid_country",
 ]
 
 DYNAMIC_ATTR_LIST = [
-"day_active_power_peak",
-"reactive_power",
-"power_factor",
-"efficiency",
-"grid_frequency",
-"grid_voltage",
-"grid_current",
-"line_voltage_A_B",
-"line_voltage_B_C",
-"line_voltage_C_A",
-"phase_A_voltage",
-"phase_B_voltage",
-"phase_C_voltage",
-"phase_A_current",
-"phase_B_current",
-"phase_C_current",
-"power_meter_active_power",
-"input_power",
-"grid_A_voltage",
-"grid_B_voltage",
-"grid_C_voltage",
-"active_grid_A_current",
-"active_grid_B_current",
-"active_grid_C_current",
-"active_grid_power_factor",
-"active_grid_frequency",
-"grid_exported_energy",
-"grid_accumulated_energy",
-"active_grid_A_B_voltage",
-"active_grid_B_C_voltage",
-"active_grid_C_A_voltage",
-"active_grid_A_power",
-"active_grid_B_power",
-"active_grid_C_power",
-"startup_time",
-"shutdown_time",
-"internal_temperature",
-"device_status",
-"system_time",
+    "day_active_power_peak",
+    "reactive_power",
+    "power_factor",
+    "efficiency",
+    "grid_frequency",
+    "grid_voltage",
+    "grid_current",
+    "line_voltage_A_B",
+    "line_voltage_B_C",
+    "line_voltage_C_A",
+    "phase_A_voltage",
+    "phase_B_voltage",
+    "phase_C_voltage",
+    "phase_A_current",
+    "phase_B_current",
+    "phase_C_current",
+    "power_meter_active_power",
+    "input_power",
+    "grid_A_voltage",
+    "grid_B_voltage",
+    "grid_C_voltage",
+    "active_grid_A_current",
+    "active_grid_B_current",
+    "active_grid_C_current",
+    "active_grid_power_factor",
+    "active_grid_frequency",
+    "grid_exported_energy",
+    "grid_accumulated_energy",
+    "active_grid_A_B_voltage",
+    "active_grid_B_C_voltage",
+    "active_grid_C_A_voltage",
+    "active_grid_A_power",
+    "active_grid_B_power",
+    "active_grid_C_power",
+    "startup_time",
+    "shutdown_time",
+    "internal_temperature",
+    "device_status",
+    "system_time",
 ]
 
 ATTR_DAILY_YIELD = "daily_yield_energy"
 ATTR_ACCUMULATED_YIELD = "accumulated_yield_energy"
 
 ENTITY_SENSOR_LIST = [
-        ATTR_DAILY_YIELD,
-        ATTR_ACCUMULATED_YIELD,
-        ]
+    ATTR_DAILY_YIELD,
+    ATTR_ACCUMULATED_YIELD,
+]
 
 
 ATTR_NB_OPTIMIZERS = "nb_optimizers"
@@ -118,28 +118,28 @@ ATTR_STORAGE_TOTAL_CHARGE = "storage_total_charge"
 ATTR_STORAGE_TOTAL_DISCHARGE = "storage_total_discharge"
 
 BATTERY_ENTITY_SENSOR_LIST = [
-ATTR_STORAGE_CHARGE_DISCHARGE_POWER,
-ATTR_STORAGE_TOTAL_CHARGE,
-ATTR_STORAGE_TOTAL_DISCHARGE,
-        ]
+    ATTR_STORAGE_CHARGE_DISCHARGE_POWER,
+    ATTR_STORAGE_TOTAL_CHARGE,
+    ATTR_STORAGE_TOTAL_DISCHARGE,
+]
 
 
 BATTERY_ATTR_LIST = [
-"storage_running_status",
-"storage_current_day_charge_capacity",
-"storage_current_day_discharge_capacity",
-"storage_working_mode_a",
-"storage_unit_1_working_mode_b",
-"storage_time_of_use_price",
-"storage_lcoe",
-"storage_maximum_charging_power",
-"storage_maximum_discharging_power",
-"storage_power_limit_grid_tied_point",
-"storage_charging_cutoff_capacity",
-"storage_discharging_cutoff_capacity",
-"storage_forced_charging_and_discharging_period",
-"storage_forced_charging_and_discharging_power",
-"storage_state_of_capacity",
+    "storage_running_status",
+    "storage_current_day_charge_capacity",
+    "storage_current_day_discharge_capacity",
+    "storage_working_mode_a",
+    "storage_unit_1_working_mode_b",
+    "storage_time_of_use_price",
+    "storage_lcoe",
+    "storage_maximum_charging_power",
+    "storage_maximum_discharging_power",
+    "storage_power_limit_grid_tied_point",
+    "storage_charging_cutoff_capacity",
+    "storage_discharging_cutoff_capacity",
+    "storage_forced_charging_and_discharging_period",
+    "storage_forced_charging_and_discharging_power",
+    "storage_state_of_capacity",
 ]
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
@@ -159,7 +159,9 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     static_attributes = {}
 
     try:
-        inverter = AsyncHuaweiSolar(host=config[CONF_HOST], loop=hass.loop, slave=config[CONF_SLAVE])
+        inverter = AsyncHuaweiSolar(
+            host=config[CONF_HOST], loop=hass.loop, slave=config[CONF_SLAVE]
+        )
 
         for register in STATIC_ATTR_LIST:
             static_attributes[register] = (await inverter.get(register)).value
@@ -197,7 +199,6 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
         battery_installed=config[CONF_BATTERY],
         static_attributes=static_attributes,
     )
-
 
     entities = [
         huawei_solar_sensor,
@@ -256,7 +257,9 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
 
 
 class HuaweiSolarSensor(Entity):
-    def __init__(self, inverter, optimizers_installed, battery_installed, static_attributes):
+    def __init__(
+        self, inverter, optimizers_installed, battery_installed, static_attributes
+    ):
         self._inverter = inverter
         self._optimizers_installed = optimizers_installed
         self._battery_installed = battery_installed
@@ -267,7 +270,9 @@ class HuaweiSolarSensor(Entity):
         self._state = None
         self.sensor_states = {}
         self._attributes = static_attributes
-        self._name = self._attributes["model_name"] + "_" + self._attributes["serial_number"]
+        self._name = (
+            self._attributes["model_name"] + "_" + self._attributes["serial_number"]
+        )
         self._unique_id = self._name
         self._pv_strings_voltage = [None] * self._attributes[ATTR_NB_PV_STRINGS]
         self._pv_strings_current = [None] * self._attributes[ATTR_NB_PV_STRINGS]
@@ -300,17 +305,19 @@ class HuaweiSolarSensor(Entity):
 
     @property
     def device_state_attributes(self):
-        attribute_list = DYNAMIC_ATTR_LIST+STATIC_ATTR_LIST+STATIC_ATTR_GRID_LIST
+        attribute_list = DYNAMIC_ATTR_LIST + STATIC_ATTR_LIST + STATIC_ATTR_GRID_LIST
         attribute_list.append(ATTR_NB_PV_STRINGS)
 
         if self._optimizers_installed:
-            attribute_list = attribute_list + [ATTR_NB_OPTIMIZERS, ATTR_NB_ONLINE_OPTIMIZERS]
+            attribute_list = attribute_list + [
+                ATTR_NB_OPTIMIZERS,
+                ATTR_NB_ONLINE_OPTIMIZERS,
+            ]
 
         if self._battery_installed:
             attribute_list = attribute_list + BATTERY_ATTR_LIST
 
-        attributes = { key : self._attributes.get(key, None) for key in attribute_list}
-
+        attributes = {key: self._attributes.get(key, None) for key in attribute_list}
 
         for i in range(int(self._attributes[ATTR_NB_PV_STRINGS] or 0)):
             attributes[f"pv_string_{i+1:02}_voltage"] = self._pv_strings_voltage[i]
@@ -344,15 +351,11 @@ class HuaweiSolarSensor(Entity):
         for i in range(int(self._attributes[ATTR_NB_PV_STRINGS] or 0)):
             try:
                 register = f"pv_{i+1:02}_voltage"
-                self._pv_strings_voltage[i] = (
-                    await self._inverter.get(register)
-                ).value
+                self._pv_strings_voltage[i] = (await self._inverter.get(register)).value
                 _LOGGER.debug("get register: %s", register)
                 await asyncio.sleep(DEFAULT_COOLDOWN_INTERVAL)
                 register = f"pv_{i+1:02}_current"
-                self._pv_strings_current[i] = (
-                    await self._inverter.get(register)
-                ).value
+                self._pv_strings_current[i] = (await self._inverter.get(register)).value
                 _LOGGER.debug("get register: %s", register)
                 await asyncio.sleep(DEFAULT_COOLDOWN_INTERVAL)
             except (ReadException, ConnectionException) as ex:
@@ -361,11 +364,15 @@ class HuaweiSolarSensor(Entity):
 
         if self._optimizers_installed:
             try:
-                self._attributes[ATTR_NB_ONLINE_OPTIMIZERS] = (await self._inverter.get(ATTR_NB_ONLINE_OPTIMIZERS)).value
+                self._attributes[ATTR_NB_ONLINE_OPTIMIZERS] = (
+                    await self._inverter.get(ATTR_NB_ONLINE_OPTIMIZERS)
+                ).value
                 _LOGGER.debug("get register: %s", register)
                 await asyncio.sleep(DEFAULT_COOLDOWN_INTERVAL)
             except (ReadException, ConnectionException) as ex:
-                _LOGGER.error("could not get register '%s': %s", ATTR_NB_ONLINE_OPTIMIZERS, ex)
+                _LOGGER.error(
+                    "could not get register '%s': %s", ATTR_NB_ONLINE_OPTIMIZERS, ex
+                )
                 await asyncio.sleep(DEFAULT_RECONNECT_INTERVAL)
 
         # values for other entity sensors
@@ -407,7 +414,15 @@ class HuaweiSolarSensor(Entity):
 
 class HuaweiSolarEntitySensor(Entity):
     def __init__(
-        self, inverter, unit, icon, device_class, parent_sensor, register, name_prefix=None):
+        self,
+        inverter,
+        unit,
+        icon,
+        device_class,
+        parent_sensor,
+        register,
+        name_prefix=None,
+    ):
         self._inverter = inverter
         self._hidden = False
         self._available = False
@@ -416,9 +431,13 @@ class HuaweiSolarEntitySensor(Entity):
         self._parent_sensor = parent_sensor
         self._register = register
         if name_prefix:
-            self._name = name_prefix + "_" + self._parent_sensor._attributes[ATTR_SERIAL_NUMBER]
+            self._name = (
+                name_prefix + "_" + self._parent_sensor._attributes[ATTR_SERIAL_NUMBER]
+            )
         else:
-            self._name = register + "_" + self._parent_sensor._attributes[ATTR_SERIAL_NUMBER]
+            self._name = (
+                register + "_" + self._parent_sensor._attributes[ATTR_SERIAL_NUMBER]
+            )
         self._unique_id = self._name
         self._device_class = device_class
 
@@ -467,8 +486,7 @@ class HuaweiSolarDailyYieldSensor(HuaweiSolarEntitySensor):
     def device_state_attributes(self):
         system_time = self._parent_sensor._attributes.get("system_time", None)
         if system_time is not None:
-            last_reset = system_time.replace(
-                    hour=0, minute=0, second=0)
+            last_reset = system_time.replace(hour=0, minute=0, second=0)
         else:
             last_reset = None
 
